20c0db70fcb82051f4107326af7129155e00a3a1
Serosh <janmejayapanda400@gmail.com>
2026-01-01 13:03:42 +0530

    Implement MauiProgram for QuantumTeleportMAUI app

---
+++ QuantumTeleportMAUI/MauiProgram.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Logging;
+using QuantumTeleportMAUI.Services;
+
+namespace QuantumTeleportMAUI;
+
+public static class MauiProgram {
+    public static MauiApp CreateMauiApp() {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddSingleton<QuantumService>();
+        builder.Services.AddSingleton<DataService>(provider => {
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "experiments.db3");
+            return new DataService(dbPath);
+        });
+
+        return builder.Build();
+    }
+}
